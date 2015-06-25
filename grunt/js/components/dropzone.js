2341d96708e29c70960194021ca26ab27ba20bf6
Brent Roose <brent.roose@gmail.com>
2015-06-25 09:54:51 +0200

    add filesize check and translateable error

--- grunt/js/components/dropzone.js
+++ grunt/js/components/dropzone.js
@@ -1,24 +1,26 @@
 window.app = window.app || {};
 
 app.dropzone = (function($, undefined) {
+  var maxFilesize = 2,
+      errorFilesize = ($("#asset-dropzone").data('errorFilesize')).replace("%size%", ""+maxFilesize) || "This file is too big.";
 
   Dropzone.options.assetDropzone = {
-    init: function() {
-      self = this;
+    maxFilesize: maxFilesize,
+    parallelUploads : 1,
+    dictDefaultMessage : "Upload",
+    dictFileTooBig : errorFilesize,
 
+    init: function() {
       $("#dropzone-upload").click(function() {
         location.reload();
       });
     },
-    dictDefaultMessage : "Upload",
 
     success : function(file, html) {
         var elem = $(html);
         $('.gridOverview').append(elem);
         this.removeFile(file);
     },
-
-    parallelUploads : 1
   };
 
 })(jQuery);
