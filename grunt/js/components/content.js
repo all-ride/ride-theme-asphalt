4bb77edc5f36ca6506c4a36bfe25b59c17c73f83
Bart Verbruggen <bart@statik.be>
2015-11-25 15:19:16 +0100

    update rich editor js

--- grunt/js/components/content.js
+++ grunt/js/components/content.js
@@ -3,9 +3,15 @@ window.app = window.app || {};
 app.content = (function($, undefined) {
 
   var _initialize = function() {
+    var $content = $('.js-rich-content');
 
-    new SirTrevor.Editor({ el: $('.js-rich-content') });
-    
+    $content.each(function() {
+        var $element = $(this);
+        var options = $element.data('rich-content-properties');
+
+        $.extend(options, { el: $element });
+        new SirTrevor.Editor(options);
+    })
   };
 
   return {
