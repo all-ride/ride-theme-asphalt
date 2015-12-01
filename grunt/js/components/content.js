85fdef279035a61eabd0f1556816695734566d4e
Steven <steven@statik.be>
2015-12-01 12:37:40 +0100

    trying to save data

--- grunt/js/components/content.js
+++ grunt/js/components/content.js
@@ -11,7 +11,7 @@ app.content = (function($, undefined) {
         var redactorOptions = $element.data('redactor-properties');
 
         options = $.extend(options, {
-          "blockTypes": ["Wysiwyg"],
+          "blockTypes": ["Wysiwyg", "Text"],
           "el": $element
         });
 
@@ -22,7 +22,8 @@ app.content = (function($, undefined) {
 
   var _initWysiwyg = function(options) {
     var options = htmlEscape(JSON.stringify(options));
-    var textAreaTemplate = '<textarea class="wysiwyg form__text" data-redactor-properties="' + options + '"></textarea>';
+    var textAreaTemplate = '<textarea class="st-text-block wysiwyg form__text" data-redactor-properties="' + options + '"></textarea>';
+    // st-text-block class needed for save
 
     // Set options for new blocks
     SirTrevor.setBlockOptions("Wysiwyg", {
@@ -38,10 +39,34 @@ app.content = (function($, undefined) {
         title: function() { return 'Wysiwyg'; },
         editorHTML: textAreaTemplate,
         icon_name: 'text',
+        // setTextBlockHTML: function(a) {
+        //   return '<p>Test</p>';
+        // },
         loadData: function(data){
           var $editor = $(this.$editor);
-          $editor.html(data.body);
+          $editor.html(data.text);
+
+          // console.log(data.text);
         },
+        // beforeBlockRender: function(data){
+        // },
+        // save: function(data){
+        //   // console.log(data);
+        //   // console.log(this);
+
+        //   var data = this.$editor.val();
+        //   console.log(data);
+
+        //   // if (!_.isEmpty(data)) {
+        //   //   this.setData(data);
+        //   // }
+        //   // var data = this._serializeData();
+        //   // data = htmlEscape(data);
+        //   // TODO: set content as HTML content of textarea in the DOM
+        // },
+        // validateAndSaveBlock: function(block, shouldValidate) {
+        //   console.log(block);
+        // },
         onBlockRender: function(data){
           var textarea = this.$editor;
           $(textarea).initRedactor();
@@ -51,6 +76,7 @@ app.content = (function($, undefined) {
   };
 
   function htmlEscape(str) {
+    // http://stackoverflow.com/questions/1219860/html-encoding-in-javascript-jquery
     return String(str)
       .replace(/&/g, '&amp;')
       .replace(/"/g, '&quot;')
@@ -58,6 +84,15 @@ app.content = (function($, undefined) {
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;');
   }
+
+  // function htmlUnescape(value){
+  //   return String(value)
+  //       .replace(/&quot;/g, '"')
+  //       .replace(/&#39;/g, "'")
+  //       .replace(/&lt;/g, '<')
+  //       .replace(/&gt;/g, '>')
+  //       .replace(/&amp;/g, '&');
+  // }
 
   return {
     init: _initialize,
